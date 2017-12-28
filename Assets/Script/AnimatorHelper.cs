70c32c226808e53413a6b30fd747873a4d62f336
Lin--laptop <linyuan5856139@163.com>
2017-12-29 03:28:11 +0800

    test animator

---
+++ Assets/Script/AnimatorHelper.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatorHelper : MonoBehaviour
+{
+
+    private Animator mAnimator;
+    [SerializeField]
+    private string[] clips;
+    [SerializeField]
+    private int index;
+    [SerializeField]
+    private bool play;
+
+    // Use this for initialization
+    void Start()
+    {
+        this.mAnimator = this.GetComponent<Animator>();
+        this.Init();
+    }
+
+
+    void Init()
+    {
+        var allClips = this.mAnimator.runtimeAnimatorController.animationClips;
+
+        this.clips = new string[allClips.Length];
+        for (int i = 0; i < allClips.Length; i++)
+        {
+            this.clips[i] = allClips[i].name;
+        }
+    }
+
+
+    void Update()
+    {
+        if (play)
+        {
+            int num = Mathf.Clamp(index, 0, this.clips.Length);
+            this.mAnimator.Play(this.clips[num]);
+        }
+    }
+}
